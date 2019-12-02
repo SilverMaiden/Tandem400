308784449e9975bac648426e6490d8b513bb65a0
SilverMaiden <h.specials@gmail.com>
2019-12-02 14:39:08 -0800

    added routing

--- react-sorting-hat/src/App.js
+++ react-sorting-hat/src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import Welcome from './components/Welcome';
 import QuizForm from './components/QuizForm';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Welcome />
-        <QuizForm />
-      </div>
+        <Router>
+        <div className="App">
+                <Route exact path="/" component={Welcome} />
+                <Route path="/sorting_quiz" component={QuizForm} />
+        </div>
+        </Router>
     );
   }
 }
--- react-sorting-hat/src/components/Welcome.js
+++ react-sorting-hat/src/components/Welcome.js
@@ -1,10 +1,14 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 
 
 const Welcome = () => {
     return (
-        <> Welcome!
-        </>
+        <div>
+        <h5>Welcome!</h5>
+
+        <Link to="/sorting_quiz"><button> Go To Quiz </button></Link>
+        </div>
     )
 }
 
