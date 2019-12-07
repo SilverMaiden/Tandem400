4d9d88f6e0432f44c7630be18f912a01d64db911
SilverMaiden <h.specials@gmail.com>
2019-12-07 13:43:56 -0800

    More updates made to animations

--- react-sorting-hat/src/components/QuizForm.js
+++ react-sorting-hat/src/components/QuizForm.js
@@ -3,51 +3,7 @@ import {Link, Redirect} from "react-router-dom";
 import QuestionSelection from "./QuestionSelection";
 import Results from './Results';
 import { gsap, TimelineLite,TweenLite, CSSPlugin} from "gsap";
-let sortingQuestions =  [
-    {
-        question: "Which of the following would you most hate people to call you?",
-        answers: ["Ordinary", "Ignorant", "Cowardly", "Selfish"],
-        name: "qAnswer1"
-    },
-
-    {
-        question: "Given the choice, would you rather invent a potion that would guarantee you:",
-        answers: ["Glory", "Wisdom", "Love", "Power"],
-        name: "qAnswer2"
-    },
-
-    {
-        question: "How would you like to be known to history?",
-        answers: ["The Wise", "The Good", "The Bold", "The Brave"],
-        name: "qAnswer3",
-    },
-    {
-        question: "You enter an enchanted garden. What would you be most curious to examine first?",
-        answers: [
-                    "The silver leafed tree bearing golden apples",
-                    "The fat red toadstools that appear to be talking to each other",
-                    "The bubbling pool, in the depths of which something luminous is swirling",
-                    "The statue of an old wizard with a strangely twinkling eye"
-                ],
-        name: "qAnswer4"
-    },
-    {
-        question: "Four boxes are placed before you. Which would you try and open?",
-        answers: [
-                    "The small tortoiseshell box, embellished with gold, inside which some small creature seems to be squeaking.",
-                    "The gleaming jet black box with a silver lock and key, marked with a mysterious rune that you know to be the mark of Merlin.",
-                    "The ornate golden casket, standing on clawed feet, whose inscription warns that both secret knowledge and unbearable temptation lie within.",
-                    "The small pewter box, unassuming and plain, with a scratched message upon it that reads: I open only for the worthy."
-                ],
-        name: "qAnswer5"
-    },
-    {
-        question: "Which of the following do you find most difficult to deal with?",
-        answers: ["Hunger", "Cold", "Boredom", "Loneliness"],
-        name: "qAnswer6"
-    }
-]
-
+import {sortingQuestions} from "../data";
 
 class QuizForm extends Component {
     constructor(props) {
@@ -77,29 +33,26 @@ class QuizForm extends Component {
     }
 
     componentDidMount() {
+        //Animation for question fade in on component mount
         this.onLoadTimeline
             .add(TweenLite.from(this.ref1.current, 2, { opacity: 0 }))
             .play();
-        this.onLoadTimelineForm
-            .add(TweenLite.from(this.ref2.current, 2, { opacity: 0}))
+        //Animation for black form box fade in on component mount
+        /*this.onLoadTimelineForm
+            .add(TweenLite.from(this.ref2.current, 5, { opacity: 0}))
             .play();
-
+        */
+        //Mount animation for the Forms height, so it transitions to the next height
         let myBox = document.getElementsByTagName('form');
         let prevHeightVal = this.ref1.current.style.height;
           this.boxTimeline
             .add(TweenLite.set(this.ref1.current, {height: "auto"}))
-            .add(TweenLite.from(this.ref1.current,0.75, {height: prevHeightVal}))
+            .add(TweenLite.from(this.ref1.current,0.5, {height: prevHeightVal}))
           .play()
-
-
-
-
     }
 
     toggleChangeHandler = e => {
         this.setState({checked:false})
-
-
     }
 
     handleSubmit = e => {
@@ -112,7 +65,6 @@ class QuizForm extends Component {
         const val = e.target.value;
         let userAnswers = {...this.state.userAnswers};
         userAnswers[[property]] = val;
-        console.log(userAnswers);
         this.setState({userAnswers});
         const setter = () => {
             if (this.state.index < 5) {
@@ -120,10 +72,12 @@ class QuizForm extends Component {
                 this.setState({
                     index: this.state.index += 1,
                 });
+                //Animation for the transition of the form when it changes height
+                // automatically with the question sizes.
                 let myBox = document.getElementsByTagName('form');
                   this.boxTimeline
                     .add(TweenLite.set(this.ref1.current, {height: "auto"}))
-                    .add(TweenLite.from(this.ref1.current,0.75, {height: prevHeightVal}))
+                    .add(TweenLite.from(this.ref1.current,0.5, {height: prevHeightVal}))
                   .play()
             }
 
--- react-sorting-hat/src/data.js
+++ react-sorting-hat/src/data.js
@@ -56,4 +56,49 @@ export const houses = {
     }
 }
 
+export const sortingQuestions =  [
+    {
+        question: "Which of the following would you most hate people to call you?",
+        answers: ["Ordinary", "Ignorant", "Cowardly", "Selfish"],
+        name: "qAnswer1"
+    },
+
+    {
+        question: "Given the choice, would you rather invent a potion that would guarantee you:",
+        answers: ["Glory", "Wisdom", "Love", "Power"],
+        name: "qAnswer2"
+    },
+
+    {
+        question: "How would you like to be known to history?",
+        answers: ["The Wise", "The Good", "The Bold", "The Brave"],
+        name: "qAnswer3",
+    },
+    {
+        question: "You enter an enchanted garden. What would you be most curious to examine first?",
+        answers: [
+                    "The silver leafed tree bearing golden apples",
+                    "The fat red toadstools that appear to be talking to each other",
+                    "The bubbling pool, in the depths of which something luminous is swirling",
+                    "The statue of an old wizard with a strangely twinkling eye"
+                ],
+        name: "qAnswer4"
+    },
+    {
+        question: "Four boxes are placed before you. Which would you try and open?",
+        answers: [
+                    "The small tortoiseshell box, embellished with gold, inside which some small creature seems to be squeaking.",
+                    "The gleaming jet black box with a silver lock and key, marked with a mysterious rune that you know to be the mark of Merlin.",
+                    "The ornate golden casket, standing on clawed feet, whose inscription warns that both secret knowledge and unbearable temptation lie within.",
+                    "The small pewter box, unassuming and plain, with a scratched message upon it that reads: I open only for the worthy."
+                ],
+        name: "qAnswer5"
+    },
+    {
+        question: "Which of the following do you find most difficult to deal with?",
+        answers: ["Hunger", "Cold", "Boredom", "Loneliness"],
+        name: "qAnswer6"
+    }
+]
+
 
