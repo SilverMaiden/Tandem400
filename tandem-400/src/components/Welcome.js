7a342a8fe22a17d74d2e5e24bf7045365cede6f0
SilverMaiden <h.specials@gmail.com>
2020-10-26 16:31:52 -0700

    Changed quiz start button text.

--- tandem-400/src/components/Welcome.js
+++ tandem-400/src/components/Welcome.js
@@ -11,11 +11,11 @@ const Welcome = () => {
                 <div className="myBox ">
                 <div className="welcomeDiv">
                     <h2>Welcome to...</h2>
-                    <h1>The Tandem of 400!</h1>
+                    <h1>Tandem of 400!</h1>
                     <img className="sortingHat" src={blue_flowers} alt="blue flower png image" />
                     <h5> Take a break. Stay a while.</h5>
 
-                    <Link to="/quiz" style={{textDecoration: "none"}}><button className="welcomeButton"  > Take the Quiz </button></Link>
+                    <Link to="/quiz" style={{textDecoration: "none"}}><button className="welcomeButton"  > Let's Start! </button></Link>
                     </div>
                 </div>
             </div>
